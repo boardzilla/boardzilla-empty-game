205c98af79628343d21389163764f0acc19d6e7d
Andrew Hull <aghull@gmail.com>
2025-03-18 21:46:35 -0400

    remove css

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -3,7 +3,6 @@ import { render } from '@boardzilla/core';
 import { default as setup } from '../game/index.js';
 
 import './style.scss';
-import '@boardzilla/core/index.css';
 
 render(setup, {
   settings: {
